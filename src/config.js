e6268d17bc06a29e66bc548f2ec66ab86f91628c
padedou <padedou@gmail.com>
2017-10-27 21:03:01 +0300

    Adds the baseAssets array

--- src/config.js
+++ src/config.js
@@ -1,5 +1,12 @@
 export default {
   gameWidth: 760,
   gameHeight: 400,
-  localStorageName: 'phaseres6webpack'
+  localStorageName: 'phaseres6webpack',
+  baseAssets: [
+    {
+      name: 'tux',
+      type: 'spritesheet',
+      path: 'assets/images/heroe/tux.png'
+    }
+  ]
 }
