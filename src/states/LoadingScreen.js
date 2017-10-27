aa977d6cc38f9eed4e34bfd057c07a1e8a928d27
padedou <padedou@gmail.com>
2017-10-27 20:36:37 +0300

    Sets a background color and a text

--- src/states/LoadingScreen.js
+++ src/states/LoadingScreen.js
@@ -1,7 +1,10 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.State {
-  init () {}
+  init () {
+    this.stage.backgroundColor = '#42d4f4'
+    this.add.text(0, 0, 'Loading...')
+  }
 
   preload () {
   }
