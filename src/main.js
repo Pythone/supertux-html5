be72d518d199ba2048126cb4d7074de280d17542
padedou <padedou@gmail.com>
2017-10-27 20:35:51 +0300

    Adds and starts the LoadingState

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import 'pixi'
 import 'p2'
 import Phaser from 'phaser'
 
-import BootState from './states/Boot'
-import SplashState from './states/Splash'
+import LoadingState from './states/LoadingScreen'
 import GameState from './states/Game'
 
 import config from './config'
@@ -16,11 +15,10 @@ class Game extends Phaser.Game {
 
     super(width, height, Phaser.CANVAS, 'content', null)
 
-    this.state.add('Boot', BootState, false)
-    this.state.add('Splash', SplashState, false)
+    this.state.add('LoadingState', LoadingState, false)
     this.state.add('Game', GameState, false)
 
-    this.state.start('Boot')
+    this.state.start('LoadingState')
   }
 }
 
