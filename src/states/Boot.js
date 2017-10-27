1610e6d64b0da6165db9de7be2ab810d282c5628
padedou <padedou@gmail.com>
2017-10-27 14:41:25 +0300

    Removes preload() and updates the render() methods

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -8,25 +8,13 @@ export default class extends Phaser.State {
     this.fontsLoaded = this.fontsLoaded.bind(this)
   }
 
-  preload () {
-    WebFont.load({
-      google: {
-        families: ['Bangers']
-      },
-      active: this.fontsLoaded
-    })
-
-    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
-    text.anchor.setTo(0.5, 0.5)
-
-    this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
-  }
-
   render () {
+    /*
     if (this.fontsReady) {
       this.state.start('Splash')
     }
+    */
+    console.log('hello from Boot.js')
   }
 
   fontsLoaded () {
