e8d954c2917a1194a6701cdf7190669228cdf42b
padedou <padedou@gmail.com>
2017-11-02 16:55:35 +0200

    Adds constructor

--- src/states/LoadingScreen.js
+++ src/states/LoadingScreen.js
@@ -1,6 +1,10 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.State {
+  constructor(){
+    super()
+  }
+
   init () {
     this.stage.backgroundColor = '#42d4f4'
     this.add.text(0, 0, 'Loading...')
