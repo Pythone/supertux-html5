3b8a6558685381c63b2c82d588e2d3c16dc26228
padedou <padedou@gmail.com>
2017-11-15 11:27:52 +0200

    Adds tux into the world as an animating sprite

--- src/states/StateGame.js
+++ src/states/StateGame.js
@@ -1,14 +1,36 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.State {
+  constructor () {
+    super()
+    this.sprite = null
+
+    this.animationsTux = {}
+    this.animationsTux.small = {}
+    this.animationsTux.small.walk = [
+      'small/walk-0.png',
+      'small/walk-1.png',
+      'small/walk-2.png',
+      'small/walk-3.png',
+      'small/walk-4.png',
+      'small/walk-5.png',
+      'small/walk-6.png',
+      'small/walk-7.png'
+    ]
+  }
   init () {
     console.log('Hello from GameState')
   }
   preload () {}
 
   create () {
+    this.sprite = this.game.add.sprite(100, 100, 'tux')
+    this.sprite.animations.add('tux/small/walk', this.animationsTux.small.walk, 12)
+
+    this.sprite.play('tux/small/walk', 10, true)
   }
 
-  render () {
+  update () {
+        // this.sprite.play('tux/small/walk')
   }
 }
