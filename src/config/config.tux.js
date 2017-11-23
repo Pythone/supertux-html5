7d23db258f2bb7dc16bbdb4df8b4c6c98c3c54c0
padedou <padedou@gmail.com>
2017-11-23 19:47:43 +0200

    Adds animations to config.tux.js

--- src/config/config.tux.js
+++ src/config/config.tux.js
@@ -15,63 +15,74 @@ export default {
       ]
     ],
     [
-        'small/gameover',
-        [
-            'small/gameover-0.png',
-            'small/gameover-1.png'
-        ]
+      'small/gameover',
+      [
+        'small/gameover-0.png',
+        'small/gameover-1.png'
+      ]
     ],
     [
-        'small/grow',
-        [
-            'small/grow-0.png',
-            'small/grow-1.png',
-            'small/grow-2.png',
-            'small/grow-3.png',
-            'small/grow-4.png',
-            'small/grow-5.png',
-            'small/grow-6.png'
-        ]
+      'small/grow',
+      [
+        'small/grow-0.png',
+        'small/grow-1.png',
+        'small/grow-2.png',
+        'small/grow-3.png',
+        'small/grow-4.png',
+        'small/grow-5.png',
+        'small/grow-6.png'
+      ]
     ],
     [
-        'small/idle',
-        [
-            'small/idle-0.png',
-            'small/idle-1.png'
-        ]
+      'small/idle',
+      [
+        'small/idle-0.png',
+        'small/idle-1.png'
+      ]
     ],
     [
-        'small/jump',
-        [
-            'small/jump-0.png'
-        ]
+      'small/jump',
+      [
+        'small/jump-0.png'
+      ]
     ],
     [
-        'small/run',
-        [
-            'small/run-0.png',
-            'small/run-1.png',
-            'small/run-2.png',
-            'small/run-3.png',
-            'small/run-4.png',
-            'small/run-5.png',
-            'small/run-6.png',
-            'small/run-7.png',
-            'small/run-8.png',
-            'small/run-9.png',
-            'small/run-10.png',
-            'small/run-11.png',
-            'small/run-12.png',
-            'small/run-13.png',
-            'small/run-14.png',
-            'small/run-15.png'
-        ]
+      'small/run',
+      [
+        'small/run-0.png',
+        'small/run-1.png',
+        'small/run-2.png',
+        'small/run-3.png',
+        'small/run-4.png',
+        'small/run-5.png',
+        'small/run-6.png',
+        'small/run-7.png',
+        'small/run-8.png',
+        'small/run-9.png',
+        'small/run-10.png',
+        'small/run-11.png',
+        'small/run-12.png',
+        'small/run-13.png',
+        'small/run-14.png',
+        'small/run-15.png'
+      ]
     ],
     [
-        'small/stand',
-        [
-            'small/stand-0.png'
-        ]
+      'small/stand',
+      [
+        'small/stand-0.png'
+      ]
+    ],
+    [
+      'big/walk',
+      [
+        'big/walk-0.png',
+        'big/walk-1.png',
+        'big/walk-2.png',
+        'big/walk-3.png',
+        'big/walk-4.png',
+        'big/walk-5.png'
+      ]
     ]
   ]
 }
