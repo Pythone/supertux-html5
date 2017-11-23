1950ab2fde86e9d3e4c7768c65617f9f95ebaa0d
padedou <padedou@gmail.com>
2017-11-23 11:04:09 +0200

    Uses CharacterTux

--- src/states/StateGame.js
+++ src/states/StateGame.js
@@ -1,32 +1,19 @@
 import Phaser from 'phaser'
+import CharacterTux from '../sprites/characters/CharacterTux'
 
 export default class extends Phaser.State {
   constructor () {
     super()
-    this.sprite = null
-
-    this.animationsTux = {}
-    this.animationsTux.small = {}
-    this.animationsTux.small.walk = [
-      'small/walk-0.png',
-      'small/walk-1.png',
-      'small/walk-2.png',
-      'small/walk-3.png',
-      'small/walk-4.png',
-      'small/walk-5.png',
-      'small/walk-6.png',
-      'small/walk-7.png'
-    ]
+    this.tux = null
   }
   init () {
   }
   preload () {}
 
   create () {
-    this.sprite = this.game.add.sprite(100, 100, 'tux')
-    this.sprite.animations.add('tux/small/walk', this.animationsTux.small.walk, 12)
-
-    this.sprite.play('tux/small/walk', 10, true)
+    this.tux = new CharacterTux(this.game, 100, 100, 'tux')
+    this.add.existing(this.tux)
+    this.tux.play('small/walk', 10, true)
   }
 
   update () {
