29f2823c5d0761c7c2665ee4406a6b2e049a414b
padedou <padedou@gmail.com>
2017-11-30 18:55:23 +0200

    Creates CrateEmpty class

---
+++ src/sprites/objects/crates/CrateEmpty.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Sprite {
+  constructor (game, x = 0, y = 0, key = 'brick0') {
+    super(game, x, y, key)
+
+    this.game.physics.enable(this, Phaser.ARCADE)
+    this.body.allowGravity = false
+    this.body.immovable = true
+  }
+}
