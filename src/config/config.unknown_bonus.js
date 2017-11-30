ec9144779787bcb3033f126f9f0fd61020f2b15f
padedou <padedou@gmail.com>
2017-11-30 17:29:03 +0200

    Updates with linted code

--- src/config/config.unknown_bonus.js
+++ src/config/config.unknown_bonus.js
@@ -1,15 +1,15 @@
 export default {
-    animations: [
+  animations: [
     // key-value pairs, should be read as a Map
-        [
-            'shine',
-            [
-                'full-0.png',
-                'full-1.png',
-                'full-2.png',
-                'full-3.png',
-                'full-4.png'
-            ]
-        ]
+    [
+      'shine',
+      [
+        'full-0.png',
+        'full-1.png',
+        'full-2.png',
+        'full-3.png',
+        'full-4.png'
+      ]
     ]
+  ]
 }
