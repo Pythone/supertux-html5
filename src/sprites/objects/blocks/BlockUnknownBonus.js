1e5363de36b7a0b48236a20437d760adb7963650
padedou <padedou@gmail.com>
2017-11-30 16:25:27 +0200

    Creates BlockUnknownBonus class

---
+++ src/sprites/objects/blocks/BlockUnknownBonus.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+import configUnknownBonus from '../../../config/config.unknown_bonus'
+import setAnimations from '../../setAnimations'
+
+export default class extends Phaser.Sprite {
+  constructor (game, x = 0, y = 0, key) {
+    super(game, x, y, key)
+
+    setAnimations(configUnknownBonus.animations, this)
+
+    this.game.physics.enable(this, Phaser.ARCADE)
+    this.body.allowGravity = false
+    this.body.immovable = true
+  }
+
+  playAnimation () {
+    this.play('shine')
+  }
+}
