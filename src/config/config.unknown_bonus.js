b06caef4801a5fd3772d3056949a60d2556b00fd
padedou <padedou@gmail.com>
2017-11-30 15:54:22 +0200

    Creates animations config for unknows_bonus

---
+++ src/config/config.unknown_bonus.js
@@ -0,0 +1,15 @@
+export default {
+    animations: [
+    // key-value pairs, should be read as a Map
+        [
+            'shine',
+            [
+                'full-0.png',
+                'full-1.png',
+                'full-2.png',
+                'full-3.png',
+                'full-4.png'
+            ]
+        ]
+    ]
+}
