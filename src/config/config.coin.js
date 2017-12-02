7a65ece1d697e664181d3dcd92121fce6f09bdc7
padedou <padedou@gmail.com>
2017-12-02 18:49:20 +0200

    Creates config.coin.js

---
+++ src/config/config.coin.js
@@ -0,0 +1,15 @@
+export default {
+  animations: [
+    // key-value pairs, should be read as a Map
+    'coin_turning',
+    [
+      'coint-0.png',
+      'coint-2.png',
+      'coint-3.png',
+      'coint-4.png',
+      'coint-5.png',
+      'coint-6.png',
+      'coint-7.png'
+    ]
+  ]
+}
