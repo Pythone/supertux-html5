0ea14d3c332e1a3da15d6c2cb405396aeaad4a98
padedou <padedou@gmail.com>
2017-12-04 22:51:26 +0200

    - Moves Coin.js to ./coin - Fixes import typo - Removes play() call from constructor

--- src/sprites/objects/coin/Coin.js
+++ src/sprites/objects/coin/Coin.js
@@ -1,12 +1,11 @@
 import Phaser from 'phaser'
 import setAnimations from '../setAnimations'
-import configCoin from '../..config.coin.js'
+import configCoin from '../../config/config.coin.js'
 
 export default class extends Phaser.Sprite {
   constructor (game, x = 0, y = 0, assetKey = 'coin') {
     super(game, x, y, assetKey)
 
     setAnimations(configCoin.animations, this)
-    this.play('coin_turning', 5, true)
   }
 }
