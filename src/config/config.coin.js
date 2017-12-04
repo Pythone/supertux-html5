481fceef2e5d1a3dd405023e878888561211ab34
padedou <padedou@gmail.com>
2017-12-04 15:40:32 +0200

    Fixes typo

--- src/config/config.coin.js
+++ src/config/config.coin.js
@@ -1,15 +1,17 @@
 export default {
   animations: [
     // key-value pairs, should be read as a Map
-    'coin_turning',
     [
-      'coin-0.png',
-      'coin-2.png',
-      'coin-3.png',
-      'coin-4.png',
-      'coin-5.png',
-      'coin-6.png',
-      'coin-7.png'
+      'coin_turning',
+      [
+        'coin-0.png',
+        'coin-2.png',
+        'coin-3.png',
+        'coin-4.png',
+        'coin-5.png',
+        'coin-6.png',
+        'coin-7.png'
+      ]
     ]
   ]
 }
