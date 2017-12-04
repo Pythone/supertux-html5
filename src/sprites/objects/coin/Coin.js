2843c539b6062d4ba25192bfd8e4d0b9ca05fdf2
padedou <padedou@gmail.com>
2017-12-04 23:03:56 +0200

    Fixes import paths

--- src/sprites/objects/coin/Coin.js
+++ src/sprites/objects/coin/Coin.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
-import setAnimations from '../setAnimations'
-import configCoin from '../../config/config.coin.js'
+import setAnimations from '../../setAnimations'
+import configCoin from '../../../config/config.coin.js'
 
 export default class extends Phaser.Sprite {
   constructor (game, x = 0, y = 0, assetKey = 'coin') {
