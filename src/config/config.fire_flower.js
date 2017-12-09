a9ae21fa8180ac5e860153d724e05c0f644085ea
padedou <padedou@gmail.com>
2017-12-09 21:03:05 +0200

    Creates config.fire_flower.js

---
+++ src/config/config.fire_flower.js
@@ -0,0 +1,15 @@
+export default {
+  animations: [
+    // key-value pairs, should be read as a Map
+    [
+      'shine',
+      [
+        'fire_flower-0.png',
+        'fire_flower-1.png',
+        'fire_flower-2.png',
+        'fire_flower-3.png',
+        'fire_flower-4.png'
+      ]
+    ]
+  ]
+}
