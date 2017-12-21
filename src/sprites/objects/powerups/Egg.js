1cbf0a7881c907815430b45f3b52a8073c502591
padedou <padedou@gmail.com>
2017-12-22 00:24:05 +0200

    Adds 'egg' powerup

--- src/sprites/objects/powerups/Egg.js
+++ src/sprites/objects/powerups/Egg.js
@@ -5,14 +5,22 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
-  constructor (game, x = 0, y = 0, assetKey = 'egg-shade') {
+  constructor (game, x = 0, y = 0, tux, assetKey = 'egg-shade') {
     super(game, x, y, assetKey)
 
+    this.tux = tux
+
     this.game.physics.enable(this, Phaser.ARCADE)
-    this.body.allowGravity = true
+
+    this.events.onAddedToGroup.add(() => {
+      this.body.velocity.y = -100
+      // Randomly choose of the egg will move left or right
+      Math.random() < 0.5 ? this.body.velocity.x = -100 : this.body.velocity.x = 100
+    })
   }
 
   onCollision () {
+    this.tux.grow()
     this.destroy()
   }
 }
